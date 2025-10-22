2fdfad400303751b2f726fe78c245bff32da9330
Kheviinaaa <khevina.rameshar@umail.uom.ac.mu>
2025-10-22 17:52:21 +0400

    Add files via upload

---
+++ mock_data/sample_json.py
@@ -0,0 +1,6 @@
+{
+  "epics": [
+    {"epic_id": "EPC-001", "title": "Checkout", "description": "Cart + payment"},
+    {"epic_id": "EPC-002", "title": "Auth", "description": "Login/Signup"}
+  ]
+}
